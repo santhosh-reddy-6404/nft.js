e5eb5f601171bb728a3e855f59740ab31d5de3f1
Santhosh Reddy <santhoshreddy.hello@gmail.com>
2022-04-22 12:43:13 +0530

    Update ERC721FULL.sol

--- assets/ERC721FULL.sol
+++ assets/ERC721FULL.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8.0;
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; 
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract NFTjs is ERC721URIStorage, Ownable {
-
-  using Counters for Counters.Counter;
-  
-  Counters.Counter private tokenIds;
+contract NFTJS is ERC721URIStorage, Ownable {
 
   bool public publicMintable;
 
