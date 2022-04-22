9543576d7ae0f727940412d483490923aaf82ff6
Santhosh Reddy <santhoshreddy.hello@gmail.com>
2022-04-22 13:24:47 +0530

    Update ERC721FULL.sol

--- assets/ERC721FULL.sol
+++ assets/ERC721FULL.sol
@@ -6,7 +6,11 @@ import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ER
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; 
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract NFTJS is ERC721URIStorage, Ownable {
+contract NFTjs is ERC721URIStorage, Ownable {
+  
+  using SafeMath for uint;
+
+  uint private tokenIds;
 
   bool public publicMintable;
 
@@ -16,7 +20,7 @@ contract NFTJS is ERC721URIStorage, Ownable {
 
   uint public mintFee;
 
-  uint public tranferCharge;
+  uint public transferCharge;
 
   event lazyMintedAndTransferred(address minter, address buyer, uint tokenId);
 
@@ -35,10 +39,10 @@ contract NFTJS is ERC721URIStorage, Ownable {
   _;
   }
   
-  function mintNFT(address minter, string memory tokenURI) public payable onlyAllowed(msg.sender) returns (uint) { 
+  function mintNFT(address minter, string memory tokenURI) public payable onlyAllowed(msg.sender) { 
     require(msg.value == mintFee.mul(10**18), "send the exact mintFee");
-    tokenIds.increment();
-    uint256 newItemId = tokenIds.current();
+    tokenIds = tokenIds.add(1);
+    uint256 newItemId = tokenIds.sub(1);
     require(newItemId <= maxSupply, "maximum supply reached");
     _safeMint(minter, newItemId);
     _setTokenURI(newItemId, tokenURI);
@@ -57,15 +61,15 @@ contract NFTJS is ERC721URIStorage, Ownable {
   function lazyMintAndTransfer(address minter, address buyer, string memory tokenURI) external payable onlyAllowed(minter) {
     require(mintFee == 0, "lazyMint is not supported for this contract");
     require(msg.value > 0, "must send some ether");
-    tokenIds.increment();
-    uint256 newItemId = tokenIds.current();
+    tokenIds = tokenIds.add(1);
+    uint256 newItemId = tokenIds.sub(1);
     require(newItemId <= maxSupply, "maximum supply reached");
     _safeMint(buyer, newItemId);
     _setTokenURI(newItemId, tokenURI);
-    emit lazyMintedAndTransferred(minter, buyer, tokenIds.current());
+    emit lazyMintedAndTransferred(minter, buyer, newItemId);
     uint value1 = (msg.value).mul(100-transferCharge).div(100);
     uint value2 = (msg.value).mul(transferCharge).div(100);
-    payable(from).transfer(value1);
+    payable(minter).transfer(value1);
     payable(admin).transfer(value2);
  }
 
@@ -80,7 +84,7 @@ contract NFTJS is ERC721URIStorage, Ownable {
   }
 
   function totalSupply() public view returns (uint) {
-    return tokenIds.current();
+    return tokenIds;
   }
 
   function balance() public view returns (uint) {
